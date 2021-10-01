0aa864deb179ce987a6d20d528ada8d9b7df6ed8
Drell <adebayo.benjamin2001@gmail.com>
2021-10-01 18:00:36 +0100

    deleted some files

--- assets/js/main.js
+++ assets/js/main.js
@@ -131,11 +131,24 @@ let swiper = new Swiper(".discover__container",
 
 /*==================== SHOW SCROLL UP ====================*/ 
 function scrollUp(){
-    const scrollUp = document.getElementById('scroll-up');
+    const scrollUps = document.getElementsByClassName('scroll-up');
     // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+    for (let i = 0; i < scrollUps.length; i++) {
+        const scrollUp = scrollUps[i];
+
+        if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+        
+    }
 }
 window.addEventListener('scroll', scrollUp)
+
+function showWhatsapp(){
+    if(this.scrollY >= 200)
+    document.getElementsByClassName("whatsapp")[0].style.display = "flex";
+    else document.getElementsByClassName("whatsapp")[0].style.display = "none";
+
+}
+window.addEventListener('scroll', showWhatsapp)
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
